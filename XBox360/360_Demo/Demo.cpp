31cdd95cb9621f7949b9192498c83c51708320ca
Zeshan Rasul <zeshanrasul90@gmail.com>
2026-04-25 02:42:22 +0100

    Successfully load 360 texture

--- XBox360/360_Demo/Demo.cpp
+++ XBox360/360_Demo/Demo.cpp
@@ -9,48 +9,57 @@
 #include <AtgResource.h>
 #include <AtgUtil.h>
 
-const char* g_strVertexShaderProgram = 
-" float4x4 matWVP : register(c0);              "  
-"                                              "  
-" struct VS_IN                                 "  
-" {                                            " 
-"     float4 ObjPos   : POSITION;              "  // Object space position 
-"     float4 Color    : COLOR;                 "  // Vertex color                 
-" };                                           " 
-"                                              " 
-" struct VS_OUT                                " 
-" {                                            " 
-"     float4 ProjPos  : POSITION;              "  // Projected space position 
-"     float4 Color    : COLOR;                 "  
-" };                                           "  
-"                                              "  
-" VS_OUT main( VS_IN In )                      "  
-" {                                            "  
-"     VS_OUT Out;                              "  
-"     Out.ProjPos = mul( matWVP, In.ObjPos );  "  // Transform vertex into
-"     Out.Color = In.Color;                    "  // Projected space and 
-"     return Out;                              "  // Transfer color
-" } " ;  
+//--------------------------------------------------------------------------------------
+// Vertex shader
+// We use the register semantic here to directly define the input register
+// matWVP.  Conversely, we could let the HLSL compiler decide and check the
+// constant table.
+//--------------------------------------------------------------------------------------
+const CHAR*         g_strVertexShaderProgram =
+    "                                              "
+    "                                              "
+    " float4x4 matWVP : register(c0);              "
+    "                                              "
+    " struct VS_IN                                 "
+    "                                              "
+    " {                                            "
+    "     float4 ObjPos : POSITION;                "  // Object space position 
+    "     float2 UV     : TEXCOORD;                "  // Texture coordinate
+    " };                                           "
+    "                                              "
+    " struct VS_OUT                                "
+    " {                                            "
+    "     float4 ProjPos  : POSITION;              "  // Projected space position 
+    "     float2 UV       : TEXCOORD0;             "
+    " };                                           "
+    "                                              "
+    " VS_OUT main( VS_IN In )                      "
+    " {                                            "
+    "     VS_OUT Out;                              "
+    "     Out.ProjPos = mul( matWVP, In.ObjPos );  "  // Transform vertex into 
+    "     Out.UV = In.UV;                          "  // Projected space and
+    "     return Out;                              "  // Transfer UVs
+    " }                                            ";
+
 
 //-------------------------------------------------------------------------------------
 // Pixel shader
+// We use the register semantic here to directly define the input sampler
+// ColorTexture.  Conversely, we could let the HLSL compiler decide and check
+// the constant table.
 //-------------------------------------------------------------------------------------
-const char* g_strPixelShaderProgram = 
-" struct PS_IN                                 "
-" {                                            "
-"     float4 Color : COLOR;                    "  // Interpolated color from                      
-" };                                           "  // the vertex shader
-"                                              "  
-" float4 main( PS_IN In ) : COLOR              "  
-" {                                            "  
-"     return In.Color;                         "  // Output color
-" } " ;
-
-struct COLORVERTEX
-{
-    float       Position[3];
-    DWORD       Color;
-};
+const CHAR*         g_strPixelShaderProgram =
+    " sampler2D ColorTexture : register(s0);       "
+    "                                              "
+    " struct PS_IN                                 "
+    " {                                            "
+    "     float2 UV : TEXCOORD0;                   "  // Interpolated UV from
+    " };                                           "  // the vertex shader 
+    "                                              "
+    " float4 main( PS_IN In ) : COLOR              "
+    " {                                            "
+    "     return tex2D( ColorTexture, In.UV );     "  // Sample texture and output
+    " }                                            ";
 
 //-------------------------------------------------------------------------------------
 // Global variables
@@ -61,11 +70,15 @@ D3DVertexDeclaration*  g_pVertexDecl;   // Vertex format decl
 D3DVertexShader*       g_pVertexShader; // Vertex Shader
 D3DPixelShader*        g_pPixelShader;  // Pixel Shader
 
+
 XMMATRIX g_matWorld;
 XMMATRIX g_matProj;
 XMMATRIX g_matView;
 
 BOOL g_bWidescreen = TRUE;
+
+
+
 
 class Demo_360 : public ATG::Application
 {
@@ -131,6 +144,7 @@ public:
 	virtual HRESULT InitApp();
     virtual HRESULT Update();
     virtual HRESULT Render();
+
 };
 
 void _cdecl main()
@@ -139,15 +153,219 @@ void _cdecl main()
 
 	if( FAILED( app.InitD3D() ) )
         return;
+	
 
-	// Initialize the vertex buffer
-    if( FAILED( app.InitScene() ) )
-        return;
+
+	IDirect3DTexture9* pTexture;
+	IDirect3DVertexDeclaration9* pVertexDecl;
+	IDirect3DVertexShader9* pVertexShader;
+	IDirect3DPixelShader9* pPixelShader;
+	// Structure to hold vertex data
+
+	ID3DXBuffer* pShaderCode = NULL;
+    ID3DXBuffer* pErrorMsg = NULL;
+
+	 // Compile vertex shader
+    HRESULT hr = D3DXCompileShader( g_strVertexShaderProgram, ( UINT )strlen( g_strVertexShaderProgram ),
+                                    NULL, NULL, "main", "vs_2_0", 0,
+                                    &pShaderCode, &pErrorMsg, NULL );
+    if( FAILED( hr ) )
+    {
+        OutputDebugStringA( pErrorMsg ? ( CHAR* )pErrorMsg->GetBufferPointer() : "" );
+        exit( 1 );
+    }
+
+    // Create vertex shader
+  
+    g_pd3dDevice->CreateVertexShader( ( DWORD* )pShaderCode->GetBufferPointer(),
+                                      &pVertexShader );
+
+    // Shader code is no longer required
+    pShaderCode->Release();
+    pShaderCode = NULL;
+
+    // Compile pixel shader
+    hr = D3DXCompileShader( g_strPixelShaderProgram, ( UINT )strlen( g_strPixelShaderProgram ),
+                            NULL, NULL, "main", "ps_2_0", 0,
+                            &pShaderCode, &pErrorMsg, NULL );
+    if( FAILED( hr ) )
+    {
+        OutputDebugStringA( pErrorMsg ? ( CHAR* )pErrorMsg->GetBufferPointer() : "" );
+        exit( 1 );
+    }
+
+
+    g_pd3dDevice->CreatePixelShader( ( DWORD* )pShaderCode->GetBufferPointer(),
+                                     &pPixelShader );
+
+    // Shader code no longer required
+    pShaderCode->Release();
+    pShaderCode = NULL;
+
+    // Load texture
+
+    // D3DXCreateTextureFromFile loads a texture and can optionally resize it,
+    // filter it, generate mip levels, etc.  It is good for game prototyping
+    // but is not suitable for final shipping code due to load time performance
+    // reasons.
+    hr = D3DXCreateTextureFromFileEx( g_pd3dDevice, "game:\\Media\\Textures\\Rocks.tga",
+                                      D3DX_DEFAULT, D3DX_DEFAULT, D3DX_DEFAULT,
+                                      0, D3DFMT_UNKNOWN, D3DPOOL_DEFAULT,
+                                      D3DX_DEFAULT, D3DX_DEFAULT, 0, NULL, NULL,
+                                      &pTexture );
+    if( FAILED( hr ) )
+	{
+		char buffer[256];
+		sprintf( buffer, "Texture load failed. HRESULT = 0x%08X\n", hr );
+	    OutputDebugStringA( buffer );
+		DebugBreak();
+
+	}
+    // Make texture format sRGB, since the source image is encoded in sRGB space.
+    // Note: this is not the best quality way of doing this - see the ATG::ConvertTextureToGoodSRGB 
+    // function in the ATG framework for the full high-quality method.
+    pTexture->Format.SignX = GPUSIGN_GAMMA;
+    pTexture->Format.SignY = GPUSIGN_GAMMA;
+    pTexture->Format.SignZ = GPUSIGN_GAMMA;
+
+    // Define some vertices to draw
+	struct COLORVERTEX
+    {
+        FLOAT   Position[3];
+        FLOAT   UV[2];
+    };
+
+    // Triangle array width and height
+    const UINT Width = 35;
+    const UINT Height = 35;
+
+    // Number of primitives in stripped mesh
+    const UINT NumPrimitives = Width * Height * 2 + ( Height - 1 ) * 4;
+
+    // Number of vertices needed in the vertex buffer
+    UINT NumVertices = ( Width + 1 ) * ( Height + 1 );
+
+   	IDirect3DVertexBuffer9* pVertexBuffer;
+    g_pd3dDevice->CreateVertexBuffer( NumVertices * sizeof( COLORVERTEX ),
+                                      D3DUSAGE_WRITEONLY, 0, D3DPOOL_DEFAULT,
+                                      &pVertexBuffer, NULL );
+
+    // Fill the vertex buffer
+    COLORVERTEX* pVertices;
+    pVertexBuffer->Lock( 0, 0, ( VOID** )&pVertices, 0 );
+    for( UINT i = 0; i < Height + 1; i++ )
+    {
+        for( UINT j = 0; j < Width + 1; j++ )
+        {
+            XMVECTOR pos;
+            pos.x = ( ( FLOAT )j - ( FLOAT )( Width ) / 2.0f ) * 4.0f / Width;
+            pos.y = ( ( FLOAT )i - ( FLOAT )( Height ) / 2.0f ) * 4.0f / Height;
+            pos.z = 0.2f * cosf( XMVector2Length( pos ).x * 5.0f );
+            pVertices->Position[0] = pos.x;
+            pVertices->Position[1] = pos.y;
+            pVertices->Position[2] = pos.z;
+
+            FLOAT u = ( FLOAT )( j ) / Width;
+            FLOAT v = ( FLOAT )( i ) / Height;
+            pVertices->UV[0] = u;
+            pVertices->UV[1] = v;
+
+            pVertices++;
+        }
+    }
+    pVertexBuffer->Unlock();
+
+    // Number of indices needed in the index buffer
+    UINT NumIndices = ( Width + 1 ) * 2 * Height + 2 * ( Height - 1 );
+
+    // Create an index buffer
+    IDirect3DIndexBuffer9* pIndexBuffer;
+    g_pd3dDevice->CreateIndexBuffer( NumIndices * sizeof( WORD ),
+                                     D3DUSAGE_WRITEONLY, D3DFMT_INDEX16,
+                                     D3DPOOL_DEFAULT, &pIndexBuffer, NULL );
+    // Fill the index buffer
+    WORD* pIndices;
+    pIndexBuffer->Lock( 0, 0, ( VOID** )&pIndices, 0 );
+    UINT Index = 0;
+    for( INT i = 0; i < Height; i++ )
+    {
+        // Tri-stripped vertices
+        for( INT j = 0; j < Width + 1; j++ )
+        {
+            pIndices[Index++] = ( WORD )( i * ( Width + 1 ) + j );
+            pIndices[Index++] = ( WORD )( ( i + 1 ) * ( Width + 1 ) + j );
+        }
+
+        // Degenerate strip connectors
+        if( i != Height - 1 )
+        {
+            pIndices[Index++] = ( WORD )( ( i + 1 ) * ( Width + 1 ) + Width );
+            pIndices[Index++] = ( WORD )( ( i + 1 ) * ( Width + 1 ) );
+        }
+    }
+    pIndexBuffer->Unlock();
+
+    // Define the vertex elements
+    static const D3DVERTEXELEMENT9 VertexElements[3] =
+    {
+        { 0,  0, D3DDECLTYPE_FLOAT3, D3DDECLMETHOD_DEFAULT, D3DDECLUSAGE_POSITION, 0 },
+        { 0, 12, D3DDECLTYPE_FLOAT2, D3DDECLMETHOD_DEFAULT, D3DDECLUSAGE_TEXCOORD, 0 },
+        D3DDECL_END()
+    };
+
+    g_pd3dDevice->CreateVertexDeclaration( VertexElements, &pVertexDecl );
+
+	g_matWorld = XMMatrixIdentity();
+
+	// Projection Matrix
+	FLOAT fAspect = ( g_bWidescreen ) ? (16.0f / 9.0f) : (4.0f / 3.0f);
+	g_matProj = XMMatrixPerspectiveFovLH(XM_PIDIV4, fAspect, 1.0f, 200.0f);
+
+	// Initialize the view matrix
+    XMVECTOR vEyePt    = { 0.0f, 0.0f,-7.0f, 0.0f };
+    XMVECTOR vLookatPt = { 0.0f, 0.0f, 0.0f, 0.0f };
+    XMVECTOR vUp       = { 0.0f, 1.0f, 0.0f, 0.0f };
+    g_matView = XMMatrixLookAtLH( vEyePt, vLookatPt, vUp );
+
 
 	for (;;)
 	{
 		app.Update();
-		app.Render();
+		g_pd3dDevice->Clear(0L, NULL, D3DCLEAR_TARGET|D3DCLEAR_ZBUFFER|D3DCLEAR_STENCIL, D3DCOLOR_XRGB(0,0,255), 1.0f, 0L);
+
+		// Set shaders
+		g_pd3dDevice->SetVertexShader( pVertexShader );
+		g_pd3dDevice->SetPixelShader( pPixelShader );
+
+		// Set the vertex declaration
+		g_pd3dDevice->SetVertexDeclaration( pVertexDecl );
+
+		// Configure sampler 0 for trilinear sampling
+		g_pd3dDevice->SetSamplerState( 0, D3DSAMP_MIPFILTER, D3DTEXF_LINEAR );
+		g_pd3dDevice->SetSamplerState( 0, D3DSAMP_MINFILTER, D3DTEXF_LINEAR );
+		g_pd3dDevice->SetSamplerState( 0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR );
+
+		// Set texture 0
+		g_pd3dDevice->SetTexture( 0, pTexture );
+
+		// Set the stream source
+		g_pd3dDevice->SetStreamSource( 0, pVertexBuffer, 0, sizeof( COLORVERTEX ) );
+
+		// Set the index buffer
+		g_pd3dDevice->SetIndices( pIndexBuffer );
+    
+		XMMATRIX matWVP = g_matWorld * g_matView * g_matProj;
+		g_pd3dDevice->SetVertexShaderConstantF(0, (FLOAT*)&matWVP, 4);
+
+		g_pd3dDevice->DrawIndexedPrimitive(
+    D3DPT_TRIANGLESTRIP,
+    0,
+    0,
+    NumVertices,
+    0,
+    NumPrimitives
+);
+		g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
 	}
 }
 
@@ -162,19 +380,19 @@ HRESULT Demo_360::InitD3D()
 	if (!pD3D)
 		return E_FAIL;
 
-	 D3DPRESENT_PARAMETERS d3dpp; 
-    ZeroMemory( &d3dpp, sizeof(d3dpp) );
-    XVIDEO_MODE VideoMode;
-    XGetVideoMode( &VideoMode );
-    g_bWidescreen = VideoMode.fIsWideScreen;
-    d3dpp.BackBufferWidth        = min( VideoMode.dwDisplayWidth, 1280 );
-    d3dpp.BackBufferHeight       = min( VideoMode.dwDisplayHeight, 720 );
-    d3dpp.BackBufferFormat       = D3DFMT_X8R8G8B8;
-    d3dpp.BackBufferCount        = 1;
+    D3DPRESENT_PARAMETERS d3dpp;
+    ZeroMemory( &d3dpp, sizeof( d3dpp ) );
+    d3dpp.BackBufferWidth = 1280;
+    d3dpp.BackBufferHeight = 720;
+    d3dpp.BackBufferFormat =  ( D3DFORMAT )MAKESRGBFMT( D3DFMT_A8R8G8B8 );
+    d3dpp.FrontBufferFormat = ( D3DFORMAT )MAKESRGBFMT( D3DFMT_LE_X8R8G8B8 );
+    d3dpp.MultiSampleType = D3DMULTISAMPLE_NONE;
+    d3dpp.MultiSampleQuality = 0;
+    d3dpp.BackBufferCount = 1;
     d3dpp.EnableAutoDepthStencil = TRUE;
     d3dpp.AutoDepthStencilFormat = D3DFMT_D24S8;
-    d3dpp.SwapEffect             = D3DSWAPEFFECT_DISCARD;
-    d3dpp.PresentationInterval   = D3DPRESENT_INTERVAL_ONE;
+    d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;
 
 	    // Create the Direct3D device.
     if( FAILED( pD3D->CreateDevice( 0, D3DDEVTYPE_HAL, NULL,
@@ -187,100 +405,6 @@ HRESULT Demo_360::InitD3D()
 
 HRESULT Demo_360::InitScene()
 {
-	ID3DXBuffer* pVertexShaderCode;
-	ID3DXBuffer* pVertexErrorMsg;
-	HRESULT hr = D3DXCompileShader(  g_strVertexShaderProgram,
-									 (UINT)strlen( g_strVertexShaderProgram ),
-                                    NULL, 
-                                    NULL, 
-                                    "main", 
-                                    "vs_2_0", 
-                                    0, 
-                                    &pVertexShaderCode, 
-                                    &pVertexErrorMsg, 
-                                    NULL );
-	 if( FAILED(hr) )
-    {
-        if( pVertexErrorMsg )
-            OutputDebugString( (char*)pVertexErrorMsg->GetBufferPointer() );
-        return E_FAIL;
-    }    
-
-    // Create vertex shader.
-    g_pd3dDevice->CreateVertexShader( (DWORD*)pVertexShaderCode->GetBufferPointer(), 
-                                      &g_pVertexShader );
-
-    // Compile pixel shader.
-    ID3DXBuffer* pPixelShaderCode;
-    ID3DXBuffer* pPixelErrorMsg;
-    hr = D3DXCompileShader( g_strPixelShaderProgram, 
-                            (UINT)strlen( g_strPixelShaderProgram ),
-                            NULL, 
-                            NULL, 
-                            "main", 
-                            "ps_2_0", 
-                            0, 
-                            &pPixelShaderCode, 
-                            &pPixelErrorMsg,
-                            NULL );
-    if( FAILED(hr) )
-    {
-        if( pPixelErrorMsg )
-            OutputDebugString( (char*)pPixelErrorMsg->GetBufferPointer() );
-        return E_FAIL;
-    }
-
-    // Create pixel shader.
-    g_pd3dDevice->CreatePixelShader( (DWORD*)pPixelShaderCode->GetBufferPointer(), 
-                                     &g_pPixelShader );
-	
-	D3DVERTEXELEMENT9 VertexElements[3] =
-    {
-        { 0,  0, D3DDECLTYPE_FLOAT3, D3DDECLMETHOD_DEFAULT, D3DDECLUSAGE_POSITION, 0 },
-        { 0, 12, D3DDECLTYPE_D3DCOLOR, D3DDECLMETHOD_DEFAULT, D3DDECLUSAGE_COLOR, 0 },
-        D3DDECL_END()
-    };
-    g_pd3dDevice->CreateVertexDeclaration( VertexElements, &g_pVertexDecl );
-
-    // Create the vertex buffer. Here we are allocating enough memory
-    // (from the default pool) to hold all our 3 custom vertices. 
-    if( FAILED( g_pd3dDevice->CreateVertexBuffer( 3*sizeof(COLORVERTEX),
-                                                  D3DUSAGE_WRITEONLY, 
-                                                  NULL,
-                                                  D3DPOOL_MANAGED, 
-                                                  &g_pVB, 
-                                                  NULL ) ) )
-        return E_FAIL;
-
-    // Now we fill the vertex buffer. To do this, we need to Lock() the VB to
-    // gain access to the vertices. This mechanism is required because the
-    // vertex buffer may still be in use by the GPU. This can happen if the
-    // CPU gets ahead of the GPU. The GPU could still be rendering the previous
-    // frame.
-    COLORVERTEX g_Vertices[] =
-    {
-        {  0.0f, -1.1547f, 0.0f, 0xffff0000 }, // x, y, z, color
-        { -1.0f,  0.5777f, 0.0f, 0xff00ff00 },
-        {  1.0f,  0.5777f, 0.0f, 0xffffff00 },
-    };
-
-    COLORVERTEX* pVertices;
-    if( FAILED( g_pVB->Lock( 0, 0, (void**)&pVertices, 0 ) ) )
-        return E_FAIL;
-    memcpy( pVertices, g_Vertices, 3*sizeof(COLORVERTEX) );
-    g_pVB->Unlock();
-
-	g_matWorld = XMMatrixIdentity();
-
-	// Projection Matrix
-	FLOAT fAspect = ( g_bWidescreen ) ? (16.0f / 9.0f) : (4.0f / 3.0f);
-	g_matProj = XMMatrixPerspectiveFovLH(XM_PIDIV4, fAspect, 1.0f, 200.0f);
-
-	// Initialize the view matrix
-    XMVECTOR vEyePt    = { 0.0f, 0.0f,-7.0f, 0.0f };
-    XMVECTOR vLookatPt = { 0.0f, 0.0f, 0.0f, 0.0f };
-    XMVECTOR vUp       = { 0.0f, 1.0f, 0.0f, 0.0f };
-    g_matView = XMMatrixLookAtLH( vEyePt, vLookatPt, vUp );
 
     return S_OK;
 }
@@ -337,18 +461,7 @@ HRESULT Demo_360::Update()
 
 HRESULT Demo_360::Render()
 {
-	g_pd3dDevice->Clear(0L, NULL, D3DCLEAR_TARGET|D3DCLEAR_ZBUFFER|D3DCLEAR_STENCIL, D3DCOLOR_XRGB(0,0,255), 1.0f, 0L);
 
-	g_pd3dDevice->SetVertexDeclaration( g_pVertexDecl );
-    g_pd3dDevice->SetStreamSource( 0, g_pVB, 0, sizeof(COLORVERTEX) );
-    g_pd3dDevice->SetVertexShader( g_pVertexShader );
-    g_pd3dDevice->SetPixelShader( g_pPixelShader );
-    
-	XMMATRIX matWVP = g_matWorld * g_matView * g_matProj;
-	g_pd3dDevice->SetVertexShaderConstantF(0, (FLOAT*)&matWVP, 4);
-
-	g_pd3dDevice->DrawPrimitive( D3DPT_TRIANGLELIST, 0, 1);
-	g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
 
 	return S_OK;
 }
